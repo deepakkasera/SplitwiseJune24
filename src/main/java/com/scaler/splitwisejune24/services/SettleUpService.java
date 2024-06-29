3a72f4f72c7dc345f9dc570e405e3f4e2c0f391e
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-06-29 08:25:26 +0530

    Spliwise-2

--- src/main/java/com/scaler/splitwisejune24/services/SettleUpService.java
+++ src/main/java/com/scaler/splitwisejune24/services/SettleUpService.java
@@ -3,19 +3,23 @@ package com.scaler.splitwisejune24.services;
 import com.scaler.splitwisejune24.dtos.SettleUpGroupRequestDto;
 import com.scaler.splitwisejune24.models.Expense;
 import com.scaler.splitwisejune24.models.ExpenseType;
+import com.scaler.splitwisejune24.models.ExpenseUser;
 import com.scaler.splitwisejune24.models.User;
+import com.scaler.splitwisejune24.repositories.ExpenseUserRepository;
 import com.scaler.splitwisejune24.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class SettleUpService {
     private UserRepository userRepository;
+    private ExpenseUserRepository expenseUserRepository;
 
-    public SettleUpService(UserRepository userRepository) {
+    public SettleUpService(UserRepository userRepository,
+                           ExpenseUserRepository expenseUserRepository) {
         this.userRepository = userRepository;
+        this.expenseUserRepository = expenseUserRepository;
     }
 
     public List<Expense> settleUpUser(Long userId) {
@@ -35,6 +39,14 @@ public class SettleUpService {
 
         User user = optionalUser.get();
 
+        List<ExpenseUser> expenseUsers = expenseUserRepository.findAllByUser(user);
+
+        Set<Expense> expenses = new HashSet<>();
+
+        for (ExpenseUser expenseUser : expenseUsers) {
+            expenses.add(expenseUser.getExpense());
+        }
+
 
     }
 
@@ -42,3 +54,28 @@ public class SettleUpService {
         return null;
     }
 }
+
+
+
+/*
+User - Krithi (1234)
+
+Expense1 - Coffee => multiple ExpenseUsers
+Expense2 - Dinner
+Expense3 - Lunch
+Expense4 - Party
+
+
+
+User - Krithi
+
+Coffee, Kriti, 500, PAID_BY
+Coffee, Kriti, 200, HAD_TO_PAY
+Dinner, Krithi, 1000, HAD_TO_PAY
+
+
+Coffee -> Kriti, 500, PAID_BY.
+       -> Sasi, 300, PAID_BY
+       -> Deepak, 200, HAD_TO_PAY
+       -> Hitesh, 200, HAD_TO_PAY
+ */
